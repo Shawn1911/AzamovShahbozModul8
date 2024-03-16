125deb80af78f7bd9e380083062ca3f1aa9f76ed
Shawn1911 <azamovshahboz06082001@gmail.com>
2024-03-16 18:07:01 +0500

    asdasd

--- root/settings.py
+++ root/settings.py
@@ -79,13 +79,7 @@ WSGI_APPLICATION = 'root.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-#
-# }
+
 
 DATABASES = {
     'default': dj_database_url.config(default=os.getenv('DB_URL'))
